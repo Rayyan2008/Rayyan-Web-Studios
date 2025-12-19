a94472697c7c7877c5ae40dc339395a9d049ed6f
Rayyan Islam <crunchyjuniper83@gmail.com>
2025-12-20 01:25:23 +0530

    No text circle

--- components/pulsing-circle.tsx
+++ components/pulsing-circle.tsx
@@ -40,7 +40,7 @@ export default function PulsingCircle() {
           </defs>
           <text className="text-sm fill-white/80 instrument">
             <textPath href="#circle" startOffset="0%">
-              Professional Web Development • Build Trust Online • Custom Websites • Mobile Friendly •
+             
             </textPath>
           </text>
         </motion.svg>
